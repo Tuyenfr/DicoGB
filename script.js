1638cdfd508be0953fca9077d08f38738e944bb5
Tuyenfr <127860619+Tuyenfr@users.noreply.github.com>
2023-10-19 15:43:54 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+alert("bien lié")
+
+/* MON PROGRAMME : donner la définition d'un mot anglais
+
+1) Récupérer le mot saisi
+2) Envoyer le mot à l'API (https://api.dictionaryapi.dev/api/v2/entries/en/<word>)
+3) Récupérer le JSON (la donnée) en lien avec le mot
+4) Afficher les informations du mot sur ma page HTML
+5) Ajouter un lecteur pour écouter la prononciation du mot */
+
+//ETAPE 1 : RECUPERER MON MOT
+
+const form =
