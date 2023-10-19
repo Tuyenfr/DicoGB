ed6ccc06c3143b1b9fb615d8a32183f0fcb09c75
Tuyenfr <127860619+Tuyenfr@users.noreply.github.com>
2023-10-19 17:12:20 +0200

    Etapes 2 et 3

--- script.js
+++ script.js
@@ -6,12 +6,38 @@
 4) Afficher les informations du mot sur ma page HTML
 5) Ajouter un lecteur pour écouter la prononciation du mot */
 
+let wordToSearch = ""
+
 //ETAPE 1 : RECUPERER MON MOT
 
-const form = document.querySelector("#form")
+const watchSubmit = () => {
+
+const form = document.querySelector("#form") // définition de l'étape 1 mais on la lance en bas du programme
+
 form.addEventListener("submit", (event) => {
   event.preventDefault() // annule le comportement par défaut à savoir rediriger la donnée du formulaire, du coup garde le mot sur la page au lieu de l'effacer
   const data = new FormData(form) // mettre name="search" et value="" dans l'input du fichier index.html
-  const search = data.get("search")
-  console.log(search)
+  const wordToSearch = data.get("search")
+  apiCall(wordToSearch) // lancer le fetch de l'étape 2 définie après
+
 })
+}
+
+// ETAPE 2 : Envoyer le mot à l'API (requête Fetch asynchrone avec then)
+
+const apiCall = (word) => {
+  console.log("wordToSearch :", word)
+  fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`) // Pour concaténer string + interpolation utiliser les ``)
+    .then((Response) => Response.json())
+    .then((data) => {
+
+      // ETAPE 3 : récupérer la donnée
+      console.log(data)
+      const wordInformation = data[0]
+      console.log("Mot : ", wordInformation.word)
+    })
+}
+
+
+//LANCEMENT DU PROGRAMME
+watchSubmit()
